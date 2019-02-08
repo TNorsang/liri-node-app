1a06ccb756ac72b47897430280b1b5c4ad5e5add
Norsang <thenorsang@gmail.com>
2019-02-07 21:40:55 -0500

    added liri.js

--- liri.js
+++ liri.js
@@ -2,29 +2,29 @@ require("dotenv").config();
 
 var axios = require("axios");
 
-axios.get("https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx")
-  .then(function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
-    console.log(response.data);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+// axios.get("https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx")
+//   .then(function(response) {
+//     // If the axios was successful...
+//     // Then log the body from the site!
+//     // console.log(response.data);
+//   })
+//   .catch(function(error) {
+//     if (error.response) {
+//       // The request was made and the server responded with a status code
+//       // that falls out of the range of 2xx
+//       console.log(error.response.data);
+//       console.log(error.response.status);
+//       console.log(error.response.headers);
+//     } else if (error.request) {
+//       // The request was made but no response was received
+//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//       console.log(error.request);
+//     } else {
+//       // Something happened in setting up the request that triggered an Error
+//       console.log("Error", error.message);
+//     }
+//     // console.log(error.config);
+//   });
 
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
 .then(function(response) {
@@ -45,3 +45,7 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
    
   console.log(data); 
   });
+
+var search = process.argv[2];
+
+console.log(search);
